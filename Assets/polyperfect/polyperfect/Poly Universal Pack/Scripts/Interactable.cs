f9d295418489aadcb1215bd04c1034a684bc3588
Shotlight343 <shotlight343@yahoo.com>
2026-01-31 01:51:51 +0800

    cringe

---
+++ Assets/polyperfect/polyperfect/Poly Universal Pack/Scripts/Interactable.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+
+
+    public class Interactable : MonoBehaviour
+    {
+        public string message;
+        public UnityEvent Interaction;
+
+        public void Interact()
+        {
+            Interaction.Invoke();
+        }
+    }
+
